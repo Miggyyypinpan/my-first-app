62b93c30bb84a8b7864a304967f692edc2dd3b09
Miguel-Vernard Pinpin <miggypin1218@gmail.com>
2025-11-19 14:20:43 +0800

    Activity Guide 2: Progress

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from "./header/header";
+import { Skills } from "./skills/skills";
+import { Footer } from "./footer/footer";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header, Skills, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
