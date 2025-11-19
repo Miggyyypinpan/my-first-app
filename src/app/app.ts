723926ee8f532a48949413a76811e7f88313eda2
Miggyyypinpan <miggypin1218@gmail.com>
2025-11-19 16:24:52 +0800

    19/11/2025

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from "./header/header";
+import { Personal } from "./personal/personal";
 import { Skills } from "./skills/skills";
+import { Technical } from "./technical/technical";
 import { Footer } from "./footer/footer";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Skills, Footer],
+  imports: [RouterOutlet, Header, Personal, Skills, Technical, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
