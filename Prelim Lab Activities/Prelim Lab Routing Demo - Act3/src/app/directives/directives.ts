b68379bacbad4bf61e6f517ec8d6d9dceb9ec7a5
miggyyypinpan <miggypin1218@gmail.com>
2025-11-28 14:04:14 +0800

    feat: implement directives with dynamic content and styling

--- Prelim Lab Activities/Prelim Lab Routing Demo - Act3/src/app/directives/directives.ts
+++ Prelim Lab Activities/Prelim Lab Routing Demo - Act3/src/app/directives/directives.ts
@@ -1,11 +1,36 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-directives',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './directives.html',
   styleUrl: './directives.css',
 })
 export class Directives {
+  isStaticNoteVisible = false;
+  isNoteVisible = true;
+  isParagraphVisible = true;
 
+  showNote() {
+    this.isNoteVisible = true;
+  }
+  hideNote() {
+    this.isNoteVisible = false;
+  }
+  toggleNote() {
+    this.isParagraphVisible = !this.isParagraphVisible;
+  }
+  monthNameStatic: string = "Mar";
+  monthNameDynamic: string = "Mar";
+
+  cityList : String[] =["Angeles", "San Fernando", "Mabalacat", "Tarlac", "Bataan"];
+
+  studentList: any[] = [
+    {stud_name: 'D Esquivel', course: 'Web Development', isActive:false},
+    {stud_name: 'J Dizon', course: 'Network Administration', isActive:false},
+    {stud_name: 'F Alejandro', course: 'Systems Development', isActive:false},
+    {stud_name: 'J David', course: 'CyberSecurity', isActive:false},
+    {stud_name: 'C Quintana', course: 'Web Development', isActive:true},
+  ]
 }
